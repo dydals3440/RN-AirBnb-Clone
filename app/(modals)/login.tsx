370293b49d30b396dfa26d133b702b3fe2a6a5ab
김용민 <dydals3440@gmail.com>
2024-03-26 21:08:12 +0900

    chore: 삭제

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,66 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser';
+import { defaultStyles } from '@/constants/Styles';
+import Colors from '@/constants/Colors';
 
 const Page = () => {
+  useWarmUpBrowser();
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <TextInput
+        autoCapitalize='none'
+        placeholder='Email'
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+      />
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+
+      <View style={styles.seperatorView}>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: '#000',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+        <Text style={styles.seperator}>or</Text>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: '#000',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+      </View>
+      <View></View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+    padding: 26,
+  },
+  seperatorView: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+    marginVertical: 30,
+  },
+  seperator: {
+    fontFamily: 'mon-sb',
+    color: Colors.grey,
+  },
+});
 
 export default Page;
