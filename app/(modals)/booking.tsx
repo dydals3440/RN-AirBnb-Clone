6e027a50cab906e1c7a04a6c2e45941765de0162
김용민 <dydals3440@gmail.com>
2024-04-02 21:55:05 +0900

    feat: booking modal

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -1,20 +1,150 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Touchable,
+  TextInput,
+  ScrollView,
+  Image,
+} from 'react-native';
 import React, { useState } from 'react';
 import { BlurView } from 'expo-blur';
-import Animated, { SlideInDown } from 'react-native-reanimated';
+import Animated, {
+  FadeIn,
+  FadeOut,
+  SlideInDown,
+} from 'react-native-reanimated';
 import { defaultStyles } from '@/constants/Styles';
 import { useRouter } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
+import Colors from '@/constants/Colors';
+import { places } from '@/assets/data/places';
+
+const AnimatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity);
 
 const Page = () => {
   const router = useRouter();
   const [openCard, setOpenCard] = useState(0);
   const [selectedPlace, setSelectedPlace] = useState(0);
 
-  const onClearAll = () => {};
+  const onClearAll = () => {
+    setSelectedPlace(0);
+    setOpenCard(0);
+  };
   return (
     <BlurView intensity={70} style={styles.container} tint='light'>
-      <Text>BOOK</Text>
+      {/* Where */}
+      <View style={styles.card}>
+        {openCard != 0 && (
+          <AnimatedTouchableOpacity
+            onPress={() => setOpenCard(0)}
+            style={styles.cardPreview}
+            entering={FadeIn.duration(200)}
+            exiting={FadeOut.duration(200)}
+          >
+            <Text style={styles.previewText}>Where</Text>
+            <Text style={styles.previewData}>I'm flexible</Text>
+          </AnimatedTouchableOpacity>
+        )}
+
+        {openCard === 0 && (
+          <>
+            <Animated.Text entering={FadeIn} style={styles.cardHeader}>
+              Where To?
+            </Animated.Text>
+            <Animated.View style={styles.cardBody}>
+              <View style={styles.searchSection}>
+                <Ionicons
+                  style={styles.searchIcon}
+                  name='search-outline'
+                  size={20}
+                />
+                <TextInput
+                  style={styles.inputField}
+                  placeholder='Search destination'
+                  placeholderTextColor={Colors.grey}
+                />
+              </View>
+            </Animated.View>
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{
+                gap: 25,
+                paddingLeft: 20,
+                marginBottom: 30,
+              }}
+            >
+              {places.map((item, index) => (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => setSelectedPlace(index)}
+                >
+                  <Image
+                    source={item.img}
+                    style={
+                      selectedPlace === index
+                        ? styles.placeSelected
+                        : styles.place
+                    }
+                  />
+                  <Text style={{ fontFamily: 'mon', paddingTop: 6 }}>
+                    {item.title}
+                  </Text>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+          </>
+        )}
+      </View>
+
+      {/* When */}
+      <View style={styles.card}>
+        {openCard != 1 && (
+          <AnimatedTouchableOpacity
+            onPress={() => setOpenCard(1)}
+            style={styles.cardPreview}
+            entering={FadeIn.duration(200)}
+            exiting={FadeOut.duration(200)}
+          >
+            <Text style={styles.previewText}>When</Text>
+            <Text style={styles.previewData}>Any week</Text>
+          </AnimatedTouchableOpacity>
+        )}
+        {openCard === 1 && (
+          <>
+            <Animated.Text entering={FadeIn} style={styles.cardHeader}>
+              When's your trip?
+            </Animated.Text>
+            <Animated.View entering={FadeIn}></Animated.View>
+          </>
+        )}
+      </View>
+
+      {/* Who */}
+      <View style={styles.card}>
+        {openCard != 2 && (
+          <AnimatedTouchableOpacity
+            onPress={() => setOpenCard(2)}
+            style={styles.cardPreview}
+            entering={FadeIn.duration(200)}
+            exiting={FadeOut.duration(200)}
+          >
+            <Text style={styles.previewText}>Who</Text>
+            <Text style={styles.previewData}>Add guests</Text>
+          </AnimatedTouchableOpacity>
+        )}
+        {openCard === 2 && (
+          <>
+            <Animated.Text entering={FadeIn} style={styles.cardHeader}>
+              Who's Coming
+            </Animated.Text>
+            <Animated.View></Animated.View>
+          </>
+        )}
+      </View>
 
       {/* FOOTER */}
       <Animated.View
@@ -66,5 +196,73 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 100,
+  },
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 14,
+    margin: 10,
+    elevation: 4,
+    shadowColor: '#000',
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    gap: 20,
+  },
+  previewText: {
+    fontFamily: 'mon-sb',
+    fontSize: 14,
+    color: Colors.grey,
+  },
+  previewData: {
+    fontFamily: 'mon-sb',
+    fontSize: 14,
+    color: Colors.dark,
+  },
+  cardPreview: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  cardHeader: {
+    fontFamily: 'mon-b',
+    fontSize: 24,
+    padding: 20,
+  },
+  cardBody: {
+    paddingHorizontal: 20,
+  },
+  searchSection: {
+    height: 50,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: '#ABABAB',
+    borderRadius: 8,
+    backgroundColor: '#fff',
+    alignContent: 'center',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  inputField: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  searchIcon: {
+    padding: 10,
+  },
+  place: {
+    width: 120,
+    height: 120,
+    borderRadius: 10,
+  },
+  placeSelected: {
+    width: 120,
+    height: 120,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: Colors.grey,
   },
 });
